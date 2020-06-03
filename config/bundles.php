b5b12b2b303d85becc3ef6f659300a4890cbfcd0
Yan <athenyan74@hotmail.com>
2020-06-03 16:05:10 +0200

    creation template logement unique

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
